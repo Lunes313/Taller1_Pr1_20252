af9f62f386eee69e97efd20a08ba9e0415c912e9
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2025-08-02 10:52:35 -0500

    busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse("<h1>Welcome to Home Page<h1>")
-    return render(request, 'home.html', {'name': 'Laura Restrepo'})
+    #return render(request, 'home.html', {'name': 'Laura Restrepo'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    if searchTerm:
+        movies = movies.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'movies': movies, 'searchTerm': searchTerm})
 
 def about(request):
-    return HttpResponse("<h1>About Us</h1>")
+    #return HttpResponse("<h1>About Us</h1>")
+    return render(request, 'about.html')
 
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import path
 from movie import views as movieviews
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -26,3 +29,5 @@ urlpatterns = [
     path("", movieviews.home, name="home"),
     path("about/", movieviews.about, name="about"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
